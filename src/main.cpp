ec1c972a1a655415595661d67c456887861d1c91
Anton Yanchev <ayanchev01@gmail.com>
2023-01-15 19:30:02 +0200

    make main use command line arg to run test

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,20 @@
 #include "../include/FileSystem.h"
 #include "../tests/tests.h"
 
-int main() {
+int main(int argc, char* argv[]) {
   // Create the file system
   FileSystem fs;
 
   // Create the command-line interface
   CLI cli(fs);
 
-  // Run the command-line interface
-  cli.run();
+  if (argc > 1 && std::string(argv[1]) == "--test") {
+    // Run the tests
+    runTests();
+  } else {
+    // Run the command-line interface
+    cli.run();
+  }
 
-  //Run the tests
-  //runTests();
   return 0;
 }
